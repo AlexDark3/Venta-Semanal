8137bdc6c5ca047506a91c52060fc642f2793076
AlexDark3 <97767457+AlexDark3@users.noreply.github.com>
2022-03-27 02:05:05 -0400

    Add files via upload

---
+++ Venta-Semanal.c
@@ -0,0 +1,18 @@
+// Alex Daniel Ayala Francisco 2020-0822
+// programa para las ventas semanales
+
+#include <stdio.h> //ejecuta el complilador
+
+int main()
+{
+    float Primerasemana = 15924;
+    float Segundasemana = 12783;
+    float Tercerasemana = 10456;
+    float Cuartasemana = 7864;
+    float prom;
+
+    prom = (Primerasemana + Segundasemana + Tercerasemana + Cuartasemana) / 4;
+
+    printf("El promedio de venta es: %.1f", prom);
+    return 0;
+}
